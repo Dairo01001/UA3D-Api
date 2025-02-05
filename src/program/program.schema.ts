9411f04ad545e99a146b68e95eaa30dacc24018b
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2025-02-05 17:46:15 -0500

    program bug

--- src/program/program.schema.ts
+++ src/program/program.schema.ts
@@ -1,4 +1,4 @@
-import { boolean, number, object, string, TypeOf } from 'zod';
+import { boolean, number, object, string, TypeOf } from 'zod'
 
 export const CreateProgramSchema = object({
   body: object({
@@ -11,6 +11,24 @@ export const CreateProgramSchema = object({
     }),
     status: boolean().optional(),
   }),
-});
+})
 
-export type CreateProgramInput = TypeOf<typeof CreateProgramSchema>;
+export const UpdateProgramSchema = object({
+  body: object({
+    name: string({
+      required_error: 'Name program is required',
+    })
+      .min(4, 'Program must be at least 4 characters')
+      .optional(),
+    status: boolean().optional(),
+  }),
+  params: object({
+    id: number({
+      required_error: 'Id is required',
+      invalid_type_error: 'Id must be a number',
+    }),
+  }),
+})
+
+export type CreateProgramInput = TypeOf<typeof CreateProgramSchema>
+export type UpdateProgramInput = TypeOf<typeof UpdateProgramSchema>
--- src/program/program.service.ts
+++ src/program/program.service.ts
@@ -1,14 +1,18 @@
-import { CreateProgram } from './program.models';
-import { create, getAll, getById } from './program.repository';
+import { CreateProgram, UpdateProgram } from './program.models'
+import { create, getAll, getById, update } from './program.repository'
 
 export const createProgram = async (data: CreateProgram) => {
-  return await create(data);
-};
+  return await create(data)
+}
 
 export const getAllPrograms = async () => {
-  return await getAll({ status: true }, { id: true, name: true });
-};
+  return await getAll()
+}
 
 export const getProgramById = async (id: number) => {
-  return await getById({ id }, { id: true, name: true, faculty: true });
-};
+  return await getById({ id }, { id: true, name: true, faculty: true })
+}
+
+export const updateProgram = async (id: number, data: UpdateProgram) => {
+  return await update({ id }, data)
+}
