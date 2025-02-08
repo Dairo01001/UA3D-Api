764165a5455771c48fb8f9446ca13682c15f5c5f
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2025-02-07 21:19:08 -0500

    git check port

--- src/server/routes/server.routes.ts
+++ src/server/routes/server.routes.ts
@@ -2,15 +2,23 @@ import { Router } from 'express'
 import { deserializeUser, validateResource } from '../../middlewares'
 import { adminAuthorize } from '../../middlewares/admin-authorize'
 import {
+  createdServerHandler,
   createServerHandler,
   findAllServerHandler,
   findByIdHandler,
 } from '../controller'
-import { CreateServerSchema } from '../schemas'
+import { CreatedServerSchema, CreateServerSchema } from '../schemas'
 
 export const serverRoutes = () => {
   const router = Router()
 
+  router.post(
+    '/create',
+    validateResource(CreatedServerSchema),
+    deserializeUser,
+    adminAuthorize,
+    createdServerHandler,
+  )
   router.post(
     '/',
     validateResource(CreateServerSchema),
